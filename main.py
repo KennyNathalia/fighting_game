3bc8108427d0cbeb5312591e52bfb6bbae92b5b5
Kenny Nathalia <99057679@mydavinci.nl>
2022-09-10 00:58:52 +0200

    Added background

--- main.py
+++ main.py
@@ -11,15 +11,31 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Fighter")
 
+#load background image
+bg_image = pygame.image.load("D:/laravel/fighting_game/assets/images/background/background.gif").convert_alpha()
+
+#function for drawing the background
+#blitting is drawing
+def draw_bg():
+    #scale resizes the images to the given screen width and height
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
+
 
 #game loop so that the game window doesn't shut off
 run = True
 while run:
 
+    #draw background
+    draw_bg()
+
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    #updates display
+    pygame.display.update()
 
 #exit pygame
 pygame.quit()
