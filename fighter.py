3627be7588a5cbeccce7c67e9d3dba9a73955468
Kenny Nathalia <99057679@mydavinci.nl>
2022-09-10 01:21:59 +0200

    movement for fighters added

--- fighter.py
+++ fighter.py
@@ -6,6 +6,27 @@ class Fighter():
         #creates rectangle object
         self.rect = pygame.Rect((x, y, 80, 180))
 
+    def move(self):
+        SPEED = 10
+        #dx and dy are the changes in the x and y coordinates
+        dx = 0
+        dy = 0
+
+        #get keypresses
+        key =   pygame.key.get_pressed()
+
+        #movement
+        if key[pygame.K_a]:
+            dx = -SPEED
+
+        if key[pygame.K_d]:
+            dx = SPEED  
+            
+        #update player position
+        self.rect.x += dx
+        self.rect.y += dy
+
     #draws the fighter
     def draw(self, surface): 
+        #gives the rectangle a color
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
