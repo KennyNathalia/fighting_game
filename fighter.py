843b7983719dbaa484e18f4ecbc074b1cfa8a3e3
Kenny Nathalia <99057679@mydavinci.nl>
2022-09-11 17:56:15 +0200

    health bar made and target can take damage

--- fighter.py
+++ fighter.py
@@ -7,9 +7,11 @@ class Fighter():
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
+        self.attacking = False
         self.attackType = 0
+        self.health = 100
 
-    def move(self, screen_width, screen_height, surface):
+    def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
         GRAVITY = 2
         #dx and dy are the changes in the x and y coordinates
@@ -19,29 +21,31 @@ class Fighter():
         #get keypresses
         key =   pygame.key.get_pressed()
 
-        #movement
-        if key[pygame.K_a]:
-            dx = -SPEED
+        #can only perform other actions if not currently attacking
+        if self.attacking == False:
+            #movement
+            if key[pygame.K_a]:
+                dx = -SPEED
 
-        if key[pygame.K_d]:
-            dx = SPEED 
+            if key[pygame.K_d]:
+                dx = SPEED 
 
-        #jump. You can only jump if your not currently jumping
-        if key[pygame.K_w] and self.jump == False:
-            self.vel_y = -30
-            self.jump = True
+            #jump. You can only jump if your not currently jumping
+            if key[pygame.K_w] and self.jump == False:
+                self.vel_y = -30
+                self.jump = True
 
-        #attack
-        if key[pygame.K_r] or key[pygame.K_t]:
-            self.attack(surface)
+            #attack
+            if key[pygame.K_r] or key[pygame.K_t]:
+                self.attack(surface, target)
 
-            #determine which attack was used
-            if key[pygame.K_r]:
-                self.attackType = 1
-                print('attack 1!')
-            if key[pygame.K_t]:
-                self.attackType = 2
-                print('attack 2!')
+                #determine which attack was used
+                if key[pygame.K_r]:
+                    self.attackType = 1
+                    #print('attack 1!')
+                if key[pygame.K_t]:
+                    self.attackType = 2
+                    #print('attack 2!')
 
         #apply gravity
         self.vel_y += GRAVITY
@@ -68,8 +72,15 @@ class Fighter():
         self.rect.y += dy
 
 
-    def attack(self, surface):
+    def attack(self, surface, target):
+        self.attacking = True
         attacking_rect = pygame.Rect(self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+
+        #check collision
+        if attacking_rect.colliderect(target.rect):
+            print("hit!")
+            target.health -= 10
+
         pygame.draw.rect(surface, (0, 150, 0), attacking_rect)
 
     #draws the fighter
--- main.py
+++ main.py
@@ -16,6 +16,11 @@ pygame.display.set_caption("Fighterz")
 clock = pygame.time.Clock()
 FPS = 60
 
+#define colors
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
 #load background image
 bg_image = pygame.image.load("D:/laravel/fighting_game/assets/images/background/background.gif").convert_alpha()
 
@@ -25,6 +30,15 @@ def draw_bg():
     #scale resizes the images to the given screen width and height
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_bg, (0, 0))
+
+#function for drawing the fighters health bars
+#if for example health is 50, the ratio will be 0.5
+#the health bar width changes depending on the ratio
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+
 
 #create two fighter instances
 fighter_1 = Fighter(200, 310)
@@ -40,8 +54,12 @@ while run:
     #draw background
     draw_bg()
 
+    #show stats
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
+
     #move fighters
-    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen)
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
     #fighter_2.move()
 
     #draw fighters
